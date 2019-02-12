536a05de375ed297602920d461143ff8ea059fe2
unknown <pushpendra.rathor@brancosoft.com>
2019-02-12 09:52:13 +0530

    API Commit

--- db/index.js
+++ db/index.js
@@ -5,13 +5,13 @@ var config = {
 
 
 module.exports = {
-	mongooseConnect : function(){
+	mongooseConnectlocal : function(){
 		mongoose.connect("mongodb://localhost:27017/nodeapp",config, (err)=>{
 			if(err) console.log("Error "+err);
 			else console.log("Connected to MongoDB");
 		});
 	},
-	mongooseConnectLive : function(){
+	mongooseConnect : function(){
 		mongoose.connect("mongodb+srv://ps020291:ps020291@cluster0-clgv8.mongodb.net/test?retryWrites=true",config, (err)=>{
 			if(err) console.log("Error "+err);
 			else console.log("Connected to MongoDB");
