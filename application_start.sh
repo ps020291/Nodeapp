5c97440638509c46a4c364956efdf514d52dfbbe
ps020291 <39159747+ps020291@users.noreply.github.com>
2023-05-17 17:33:30 +0530

    Update application_start.sh

--- application_start.sh
+++ application_start.sh
@@ -2,9 +2,9 @@
 sudo chmod -R 777 /home/ubuntu/nodeapp
 cd /home/ubuntu/nodeapp
 
-export nvm_dir = "$HOME/.nvm"
-[ -s  "$nvm_dir/nvm.sh" ] &&  \.  "$nvm_dir/nvm.sh"
-[ -s  "$nvm_dir/bash_completion"  ] &&  \.  "$nvm_dir/bash_completion"
+#export nvm_dir = "$HOME/.nvm"
+#[ -s  "$nvm_dir/nvm.sh" ] &&  \.  "$nvm_dir/nvm.sh"
+#[ -s  "$nvm_dir/bash_completion"  ] &&  \.  "$nvm_dir/bash_completion"
 
 npm install
 #pm2 start npm --name="nodeapp"
