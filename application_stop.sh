8cd0f30ba023079ffbcca518e59188ae6e38985f
ps020291 <39159747+ps020291@users.noreply.github.com>
2023-05-17 17:25:00 +0530

    Update application_stop.sh

--- application_stop.sh
+++ application_stop.sh
@@ -3,10 +3,10 @@
 # Stop the Node.js server or application
 
 # Example command to stop a Node.js server using PM2 process manager
-pm2 stop nodeapp
+pm2 stop server
 
 # Example command to stop a Node.js server using npm
 # Replace "node app.js" with the appropriate command to start your Node.js application
 # Replace "my_app_directory" with the appropriate directory where your Node.js application is located
 # cd /path/to/my_app_directory
-npm stop
+#npm stop
