76fcf3edc19d8ac0da7489fa957beb17fd867f2f
ps020291 <39159747+ps020291@users.noreply.github.com>
2023-05-17 17:33:07 +0530

    Update application_start.sh

--- application_start.sh
+++ application_start.sh
@@ -7,6 +7,7 @@ export nvm_dir = "$HOME/.nvm"
 [ -s  "$nvm_dir/bash_completion"  ] &&  \.  "$nvm_dir/bash_completion"
 
 npm install
-pm2 start npm --name="nodeapp"
+#pm2 start npm --name="nodeapp"
+pm2 start server.js
 
 node app.js>app.out.log. 2> app.error.log < /dev/null &
