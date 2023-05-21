2248ca89b07af8e659b4c7b7ae8172f0ef0f32c3
Pushpendra Kumar <pushpendrak@tradeindia.com>
2023-05-21 10:10:36 +0530

    changes

--- application_stop.sh
+++ application_stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Stop the Node.js application using PM2
-pm2 stop nodeapp
+pm2 stop nodeapp 2>&1
 
 # Verify if the application has stopped successfully
 pm2 describe nodeapp &> /dev/null
@@ -9,9 +9,12 @@ status=$?
 
 # Check the exit status
 if [ $status -eq 0 ]; then
-    echo "Application could not be stopped."
-    exit 1
-else
     echo "Application stopped successfully."
     exit 0
+elif [ $status -eq 1 ]; then
+    echo "Process or Namespace not found. Application may not be running."
+    exit 0
+else
+    echo "Failed to stop the application. Error code: $status"
+    exit 1
 fi
